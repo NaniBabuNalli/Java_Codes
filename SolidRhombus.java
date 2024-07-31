f349f85b3679efae0915a273b7833ae778d91c2c
nanibabunalli <nallinani66@gmail.com>
2024-07-31 22:24:22 +0530

    Add files via upload

---
+++ SolidRhombus.java
@@ -0,0 +1,28 @@
+/*
+ Input: 5
+ 
+         * * * * * 
+      * * * * * 
+    * * * * * 
+  * * * * * 
+* * * * * 
+  
+ */
+
+import java.util.Scanner;
+class  SolidRhombus{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=(n-i);j++){
+                System.out.print("  ");
+            }
+            for(j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
